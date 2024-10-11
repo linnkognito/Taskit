4f16f9da6d424de9658afea6372bbf80628a6593
Linn Johansen <linn.es.johansen@gmail.com>
2024-10-11 14:04:03 -0700

    Sass files organization

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import './css/animations.css';
-import './css/fonts.css';
-import './css/project.css';
-import './css/add-project.css';
+import './css/animations/animations.css';
+import './css/assets/fonts.css';
+import './css/projects/project.css';
+import './css/projects/add-project.css';
 import './css/vars.css';
 import './css/styles.css';
-import './css/task-card.css';
-import './css/task-form.css';
-import './css/task-checklist.css';
-import './css/task-note.css';
+import './css/tasks/task-card.css';
+import './css/tasks/task-form.css';
+import './css/tasks/task-checklist.css';
+import './css/tasks/task-note.css';
 import { App } from './js/app';
 
 let app;
