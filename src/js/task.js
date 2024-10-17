0d0a68e7a5b2a99e001ca86dace8d8626feb7956
Linn Johansen <linn.es.johansen@gmail.com>
2024-10-16 17:18:44 -0700

    Due date modal fixes

--- src/js/task.js
+++ src/js/task.js
@@ -156,9 +156,9 @@ export class Task {
   // --> Good for rendering
   calcTimeDiff(date) {
     const ms = date - new Date();
-    const mins = Math.ceil(ms / (1000 * 60));
-    const hours = Math.ceil(ms / (1000 * 60 * 60));
-    const days = Math.ceil(ms / (1000 * 60 * 60 * 24));
+    const mins = Math.floor(ms / (1000 * 60));
+    const hours = Math.floor(ms / (1000 * 60 * 60));
+    const days = Math.floor(ms / (1000 * 60 * 60 * 24));
 
     return { days, hours, mins };
   }
